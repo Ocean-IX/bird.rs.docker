893d3629ab789f3d44cac9e7ccdef73770ec2e95
RouteIX <77181250+RouteIX@users.noreply.github.com>
2021-01-22 15:26:04 +1030

    Update run.sh

--- root/run.sh
+++ root/run.sh
@@ -1,6 +1,6 @@
-[ $IPV4_ENABLE = "true"] && { sleep 5; bird -c /usr/local/etc/bird.conf -d || exit 1; }
+[ $IPV4_ENABLE = "yes"] && { sleep 5; bird -c /usr/local/etc/bird.conf -d; }
 
-[ $IPV6_ENABLE = "true" ] && { sleep 5; bird6 -c /usr/local/etc/bird6.conf -d || exit 1; }
+[ $IPV6_ENABLE = "yes" ] && { sleep 5; bird6 -c /usr/local/etc/bird6.conf -d; }
 
 # something that keep the container running
 tail -f /dev/null
